1a4f81b2ff93eea187d87a4ee49c1e08ffc4c950
MADMAN-Modding <mad@kmdtech.net>
2023-12-01 16:06:55 -0500

    Finished except for fine tuning css

--- streamOverlay.php
+++ streamOverlay.php
@@ -7,11 +7,35 @@
     <title>DCScoreboardOverlay</title>
 
     <!-- These will be echoed by php, but yeah this should work hopefully -->
-    <h2 id="leftNumber">1</h2>
-    <h2 id="rightNumber">2</h2>
+    <!-- <h2 id="leftNumber">1</h2>
+    <h2 id="rightNumber">2</h2> -->
+
+    <?php
+            $csv = array();
+
+            if(($handle = fopen("data.csv", "r")) !== FALSE) {
+                while (($data = fgetcsv($handle, 1000, ",")) !== FALSE) {
+                    $csv[] = $data;
+                }
+            }
+
+            fclose($handle);
+
+
+            // This is for if there were multiple rows
+            // for ($i = 1; $i < count($csv); $i++) {
+                for ($ii = 0; $ii <=count($csv[1]); $ii++) {
+                    echo "<h1 class=" . $csv[0][$ii] . ">" . $csv[1][$ii] . "</h1>";
+                    
+                }
+            // }
+
+        ?>
+
     <style>
         body {
             background-color: antiquewhite;
+
         }
 
         #logo {
@@ -21,24 +45,8 @@
             width: 10%;
 
         }
-        #leftScore{
-            position:absolute;
-            top:55px;
-            left:397px;
-            width: 50px;
-            height: 50px;
-            background-color: blue;
-        }
-        #rightScore{
-            position:absolute;
-            top:55px;
-            right:496.5px;
-            width: 50px;
-            height: 50px;
-            background-color: blue;
-        }
 
-        #leftNumber {
+        .score1 {
             position:absolute;
             top:55px;
             left:397px;
@@ -48,13 +56,33 @@
         }
 
 
-        #rightNumber {
+        .score2  {
             position:absolute;
             z-index: 1000;
             top:55px;
-            right:496.5px;
+            right:367.5px;
             width: 50px;
             height: 50px;
+        }
+           
+        .teamName1  {
+            position:absolute;
+            z-index: 1000;
+            top:55px;
+            left:467.5px;
+            width: 50px;
+            height: 50px;
+            color: white;
+        }
+
+        .teamName2  {
+            position:absolute;
+            z-index: 1000;
+            top:55px;
+            right:507.5px;
+            width: 50px;
+            height: 50px;
+            color: white;
         }
         
 
