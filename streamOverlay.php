c5da14733413b6dcf1218b638312f87baf3c8f8b
MADMAN-Modding <mad@kmdtech.net>
2023-12-03 10:43:38 -0500

    Can change the overlay image check comments in php

--- streamOverlay.php
+++ streamOverlay.php
@@ -24,11 +24,37 @@
 
         // This is for if there were multiple rows
         // for ($i = 1; $i < count($csv); $i++) {
-            for ($ii = 0; $ii <=count($csv[1]); $ii++) {
+            for ($ii = 0; $ii < count($csv[1])-1; $ii++) {
                 echo "<h1 class=" . $csv[0][$ii] . ">" . $csv[1][$ii] . "</h1>";
-                
             }
         // }
+
+        // All of these read the overlay and use a different image for the middle depending on what it is, none of these are case sensitive
+        // If no correct arguement is found, it will default to the DC logo
+
+        // For the DC logo use DC
+        if (strtoupper($csv[1][4]) == "DC") {
+            echo '<img id="logo" src="Overlay/Logo.png">';
+        } 
+        
+        // For SSBU use SSBU or Super Smash Bros. Ultimate
+        elseif (strtoupper($csv[1][4]) == "SSBU" || strtolower($csv[1][4]) == "super smash bros. ultimate") {
+            echo '<img id="logo" src="Overlay/SSBU.png">';
+        } 
+        
+        // For Rocket League use RL or Rocket League
+        elseif (strtoupper($csv[1][4]) == "RL" || strtolower($csv[1][4]) == "rocket league") {
+            echo '<img id="logo" src="Overlay/rocketLeague.png">';
+        }
+
+        // For Splatoon use SPLAT or Splatoon
+        elseif (strtoupper($csv[1][4]) == "SPLAT" || strtolower($csv[1][4]) == "splatoon") {
+            echo '<img id="logo" src="Overlay/Splatoon.png">';
+        }
+
+        else {
+            echo '<img id="logo" class="fallback" src="Overlay/Logo.png">';
+        }
 
     ?>
 
@@ -90,7 +116,6 @@
 
 <body>
     <img src="Overlay/scoreboardNew.png">
-    <img id="logo" src="Overlay/Logo.png">
     <div id="leftScore"></div>
     <div id="rightScore"></div>
    
