1624732516f99d788939b901600fd29e45ddade7
MADMAN-Modding <mad@kmdtech.net>
2023-12-10 13:05:16 -0500

    Finished the quickStart and run files

--- quickStart.ps1
+++ quickStart.ps1
@@ -1,54 +1,100 @@
-$path = "C:\Users\$env:UserName\Documents\Overlay\"
+# The variable for where the data will be kept for the overlay
+$data = "C:\Users\$env:UserName\Documents\Overlay\"
 
+# The variable where the php server will go
+$phpPath = "C:\Users\$env:UserName\Documents\php"
+
+# Asks the user if they've installed git
 $gitInstalled = Read-Host "Do you have git installed? [Y/N]";
 
+# Checks the user's resposne
 if ($gitInstalled.ToLower() -eq "y") {
-    git.exe clone https://github.com/HeinzEric/FalconsEsportsOverlays.git $path;
-} else {
+    # Removes the directory incase something is there already
+    Remove-Item $data -r -force;
+    
+    git.exe clone "https://github.com/HeinzEric/FalconsEsportsOverlays.git" $data;
+} elseif ($gitInstalled.ToLower() -eq "n") {
+
+    # Asks if the user wants to download git
     $gitDownload = Read-Host "Download and install git? [Y/N]";
 
+    # Checks the user's response
     if ($gitDownload.ToLower() -eq "y") {
 
+        # Opens the git download page for windows
         Start-Process "https://git-scm.com/download/win"
 
-        Write-Host "Download git for your respective version, then run the installer, the default options for git who work for this just fine";
-
-        # Removes the directory incase something is there already
-        Remove-Item $path -r -force;
+        # Just outputs texts
+        Write-Host "Download git for your respective version, then run the installer, the default options for git will work for this just fine";
 
         # Waits for user input
         Read-Host "Press enter when git is installed";
 
+        # Removes the directory incase something is there already
+        Remove-Item $data -r -force;
+
         # Clones the repo
-        git.exe clone https://github.com/HeinzEric/FalconsEsportsOverlays.git $path;
+        git.exe clone "https://github.com/HeinzEric/FalconsEsportsOverlays.git" $data;
     } elseif ($gitDownload.ToLower() -eq "n") {
         
+        # Waits for user input
+        Read-Host "Press enter to download the latest release"
+
         # Removes the directory incase something is there already
-        Remove-Item $path -r -force;
+        Remove-Item $data -r -force;
 
         # Creates the directory
-        mkdir $path;
+        mkdir $data;
 
-        # Downloads the latest release
-        Start-BitsTransfer -Source "https://github.com/HeinzEric/FalconsEsportsOverlays/releases/download/Release/FalconsEsportsOverlays-main.zip" -Destination "$path\main.zip";
-
-        # Cds to the correct one
-        Set-Location $path;
+        # Cds to the correct directory
+        Set-Location $data;
         
+        # Downloads the latest release
+        Start-BitsTransfer -Source "https://github.com/HeinzEric/FalconsEsportsOverlays/releases/download/v1.2/FalconsEsportsOverlays-main.zip" -Destination "main.zip";
+
         # Extracts the file
-        Expand-Archive -LiteralPath $path\main.zip -DestinationPath $path;
+        Expand-Archive -LiteralPath "$data\main.zip" -DestinationPath $data;
 
         # Moves files to the root
-        Move-Item -Path $path\FalconsEsportsOverlays-main\* -Destination $path;
+        Move-Item -Path "$data\FalconsEsportsOverlays-main\*" -Destination $data;
         
         # Removes the old directory
-        Remove-Item FalconsEsportsOverlays-main;
+        Remove-Item -r -force "FalconsEsportsOverlays-main";
 
-        # REMOVE BEFORE PUSH
-        Set-Location D:\Coding\Web\FalconsEsportsOverlays;
+        # Removes the downloaded zip
+        Remove-Item -r -force "main.zip"
     }
 }
 
+# Waits for input before continuing the script
+Read-Host "Press enter to download php"
 
+# Delets current php stuff if there is any
+Remove-Item $phpPath -r -force 
 
-# Start-BitsTransfer -Source "https://codeload.github.com/HeinzEric/FalconsEsportsOverlays/zip/refs/heads/main" -Destination "C:\Users\$env:UserName\Documents\Overlay\main.zip"
+# Makes the php directory
+mkdir $phpPath;
+
+# Starts the php download
+Start-BitsTransfer -Source "https://windows.php.net/downloads/releases/php-8.3.0-nts-Win32-vs16-x64.zip" -Destination $phpPath
+
+# Sets the location to do the php downloading and extracting
+Set-Location $phpPath;
+
+# Extracts PHP
+Expand-Archive -LiteralPath "php-8.3.0-nts-Win32-vs16-x64.zip" -DestinationPath ".";
+
+# Removes the downloaded zip
+Remove-Item "php-8.3.0-nts-Win32-vs16-x64.zip" -r -force
+
+# Simply waits for any kind of input
+Read-Host "Press enter to start the php server on localhost:8080";
+
+# Sets the propper location for the files
+Set-Location $data
+
+# Informs the user of how they can run the server in the future
+Write-Host "You can always run the server with ease using the run.ps1 file in $data"
+
+# Runs the php server
+..\php\php.exe -S localhost:8080
---
+++ run.ps1
@@ -0,0 +1,5 @@
+# Sets the propper location for the files
+Set-Location "C:\Users\$env:UserName\Documents\Overlay\"
+
+# Starts the server
+..\php\php.exe -S localhost:8080
