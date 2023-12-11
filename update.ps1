01d42616cda66ae201018bdf2b3bdb7e1ab3928f
MADMAN-Modding <mad@kmdtech.net>
2023-12-11 15:04:47 -0500

    Fixed powershell should be good now

--- update.ps1
+++ update.ps1
@@ -1,3 +1,9 @@
 Read-Host "Press enter to begin the download"
 
+mkdir "C:\Users\$env:UserName\Documents\Overlay\"
+
 Start-BitsTransfer -Source "https://raw.githubusercontent.com/HeinzEric/FalconsEsportsOverlays/main/download.ps1" -Destination "C:\Users\$env:UserName\Documents\Overlay\download.ps1"
+
+Set-Location "C:\Users\$env:UserName\Documents\Overlay\"
+
+.\download.ps1
