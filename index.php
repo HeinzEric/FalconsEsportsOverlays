cce575b9e1bc9ff11a944821963533748195fb8b
MADMAN-Modding <mad@kmdtech.net>
2023-12-13 21:34:42 -0500

    Partly done with controls page

--- index.php
+++ index.php
@@ -7,21 +7,20 @@
     <title>DCScoreboardOverlay</title>
 
     <?php
-        $csv = array();
+        $csvArray = array();
 
         if(($handle = fopen("data.csv", "r")) !== FALSE) {
             while (($data = fgetcsv($handle, 1000, ",")) !== FALSE) {
-                $csv[] = $data;
+                $csvArray[] = $data;
             }
         }
 
         fclose($handle);
 
-
         // This is for if there were multiple rows
-        // for ($i = 1; $i < count($csv); $i++) {
-            for ($ii = 0; $ii < count($csv[1])-1; $ii++) {
-                echo "<h1 class=" . $csv[0][$ii] . ">" . $csv[1][$ii] . "</h1>";
+        // for ($i = 1; $i < count($csvArray); $i++) {
+            for ($ii = 0; $ii < count($csvArray[1])-1; $ii++) {
+                echo "<h1 class=" . $csvArray[0][$ii] . ">" . $csvArray[1][$ii] . "</h1>";
             }
         // }
 
@@ -29,26 +28,26 @@
         // If no correct arguement is found, it will default to the DC logo
 
         // For the DC logo use DC
-        if (strtoupper($csv[1][4]) == "DC") {
+        if (strtoupper($csvArray[1][4]) == "DC") {
             echo '<img id="logo" src="images/Logo.png">';
         } 
         
         // For SSBU use SSBU or Super Smash Bros. Ultimate
-        elseif (strtoupper($csv[1][4]) == "SSBU" || strtolower($csv[1][4]) == "super smash bros. ultimate") {
+        elseif (strtoupper($csvArray[1][4]) == "SSBU" || strtolower($csvArray[1][4]) == "super smash bros. ultimate") {
             echo '<img id="logo" src="images/SSBU.png">';
         } 
         
         // For Rocket League use RL or Rocket League
-        elseif (strtoupper($csv[1][4]) == "RL" || strtolower($csv[1][4]) == "rocket league") {
+        elseif (strtoupper($csvArray[1][4]) == "RL" || strtolower($csvArray[1][4]) == "rocket league") {
             echo '<img id="logo" src="images/rocketLeague.png">';
         }
 
         // For Splatoon use SPLAT or Splatoon
-        elseif (strtoupper($csv[1][4]) == "SPLAT" || strtolower($csv[1][4]) == "splatoon") {
+        elseif (strtoupper($csvArray[1][4]) == "SPLAT" || strtolower($csvArray[1][4]) == "splatoon") {
             echo '<img id="logo" src="images/Splatoon.png">';
         } 
         
-        elseif (strtoupper($csv[1][4]) == "VAL" || strtolower($csv[1][4]) == "valorant") {
+        elseif (strtoupper($csvArray[1][4]) == "VAL" || strtolower($csvArray[1][4]) == "valorant") {
             echo '<img id="logo" src="images/Valorant.png">';
         }
 
@@ -57,16 +56,16 @@
         }
 
         // Checks the left for won or lost status
-        if(strtolower($csv[2][0]) == "won" || strtolower($csv[2][0]) == "win") {
+        if(strtolower($csvArray[2][0]) == "won" || strtolower($csvArray[2][0]) == "win") {
             echo '<img class="wonLeft" src="images/[W].png">';
-        }   elseif(strtolower($csv[2][0]) == "lost" || strtolower($csv[2][0]) == "lose") {
+        }   elseif(strtolower($csvArray[2][0]) == "lost" || strtolower($csvArray[2][0]) == "lose") {
             echo '<img class="lostLeft" src="images/[L].png">';
         }
 
         // Checks the right for won or lost status
-        if(strtolower($csv[2][1]) == "won" || strtolower($csv[2][1]) == "win") {
+        if(strtolower($csvArray[2][1]) == "won" || strtolower($csvArray[2][1]) == "win") {
             echo '<img class="wonRight" src="images/[W].png">';
-        }   elseif(strtolower($csv[2][1]) == "lost" || strtolower($csv[2][1]) == "lose") {
+        }   elseif(strtolower($csvArray[2][1]) == "lost" || strtolower($csvArray[2][1]) == "lose") {
             echo '<img class="lostRight" src="images/[L].png">';
         }
     ?>
