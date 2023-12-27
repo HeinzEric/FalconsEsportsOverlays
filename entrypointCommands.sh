537c98ee2a4325de10f58ab3bfcad2697517be04
MADMAN-Modding <mad@kmdtech.net>
2023-12-27 15:15:45 -0500

    Testing

--- entrypointCommands.sh
+++ entrypointCommands.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-git reset --hard HEAD && git clean -f -d && git pull && httpd -D FOREGROUND
+git reset --hard HEAD && git clean -f -d && git pull && chmod 777 -R /var/www/localhost/htdocs/* && httpd -D FOREGROUND
