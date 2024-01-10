3a10df7c700a45e4bf5556d62176d2c280bf8bec
MADMAN-Modding <115199645+MADMAN-Modding@users.noreply.github.com>
2024-01-10 12:30:29 +0000

    Finished JS

--- js/index.js
+++ js/index.js
@@ -5,10 +5,23 @@ setInterval(function () {
         switch (jsonData['scoreLeft']) {
             case "3":
                 document.getElementById("scoreLeftThree").style.backgroundColor = "black";
+                document.getElementById("scoreLeftTwo").style.backgroundColor = "black";
+                document.getElementById("scoreLeftOne").style.backgroundColor = "black";
                 break;
             case "2":
                 document.getElementById("scoreLeftThree").style.backgroundColor = "transparent";
+                document.getElementById("scoreLeftTwo").style.backgroundColor = "black";
+                document.getElementById("scoreLeftOne").style.backgroundColor = "black";
+            case "1":
+                document.getElementById("scoreLeftThree").style.backgroundColor = "transparent";
+                document.getElementById("scoreLeftTwo").style.backgroundColor = "transparent";
+                document.getElementById("scoreLeftOne").style.backgroundColor = "black";
+            case "0":
+                document.getElementById("scoreLeftThree").style.backgroundColor = "transparent";
+                document.getElementById("scoreLeftTwo").style.backgroundColor = "transparent";
+                document.getElementById("scoreLeftOne").style.backgroundColor = "transparent";
         }
         document.getElementById("teamNameLeft").innerHTML = jsonData["teamNameLeft"];
+        document.getElementById("teamNameRight").innerHTML = jsonData["teamNameRight"];
     })
 }, 3000)
