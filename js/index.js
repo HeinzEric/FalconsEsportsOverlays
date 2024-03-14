9069cc4f149059c7d060f6bc9737849a67e967f8
MADMAN-Modding <mad@kmdtech.net>
2024-03-14 10:25:26 -0400

    Removed useless stuff

--- js/index.js
+++ js/index.js
@@ -2,16 +2,6 @@ setInterval(function () {
     fetch('../json/overlay.json')
         .then((response) => response.json())
         .then((jsonData) => {
-            //     if (jsonData["overlay"] == "ssbu" && previousOverlay != "ssbu") {
-            //         previousOverlay = "ssbu";
-            //         console.log(previousOverlay);
-            //         xhr.open('GET', 'fileHandler.php?overlay=ssbu');
-            //     } else if (jsonData["overlay"] == "kart" && previousOverlay != "kart") {
-            //         previousOverlay = "kart";
-            //         xhr.open('GET', 'fileHandler.php?overlay=kart');
-            //     }
-            // })
-
             if (window.location.href.includes(jsonData["overlay"])) {
                 return;
             } else {
