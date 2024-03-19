fb1455a3f4780df7dc51bbe331361007f62873d7
MADMAN-Modding <115199645+MADMAN-Modding@users.noreply.github.com>
2024-03-19 11:43:14 +0000

    Fixed some stuff

---
+++ js/ssbu.js
@@ -0,0 +1,27 @@
+setInterval(function () {
+    fetch('../json/overlay.json')
+        .then((response) => response.json())
+        .then((jsonData) => {
+            const winsLeft = +jsonData["winsLeft"];
+            const winsRight = +jsonData["winsRight"];
+
+            // Update the left side rounds won
+            for (let i = 1; i <= 2; i++) {
+                document.getElementById(`leftRoundsWon${i}`).style.backgroundColor = winsLeft >= i ? "white" : "black";
+            }
+
+            // Update the right side rounds won
+            for (let i = 1; i <= 2; i++) {
+                document.getElementById(`rightRoundsWon${i}`).style.backgroundColor = winsRight >= i ? "white" : "black";
+            }
+
+            // Updates the Team Names and week
+            document.getElementById("teamNameLeft").innerHTML = jsonData["teamNameLeft"];
+            document.getElementById("teamNameRight").innerHTML = jsonData["teamNameRight"];
+            document.getElementById("week").innerHTML = jsonData["week"];
+            document.getElementById("playerNamesLeft").innerHTML = jsonData["playerNamesLeft"];
+            document.getElementById("playerNamesRight").innerHTML = jsonData["playerNamesRight"];
+            document.getElementById("scoreLeft").innerHTML = jsonData["scoreLeft"];
+            document.getElementById("scoreRight").innerHTML = jsonData["scoreRight"]
+        });
+}, 3000);
