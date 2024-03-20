5e369738b7379d0df62aaba2dfdaf7dd82d90bf1
HeinzEric <122365478+HeinzEric@users.noreply.github.com>
2024-03-20 12:59:22 -0400

    Score card update for ssbu

--- js/ssbu.js
+++ js/ssbu.js
@@ -2,18 +2,18 @@ setInterval(function () {
     fetch('../json/overlay.json')
         .then((response) => response.json())
         .then((jsonData) => {
-        //     const winsLeft = +jsonData["winsLeft"];
-        //     const winsRight = +jsonData["winsRight"];
+             const winsLeft = +jsonData["winsLeft"];
+            const winsRight = +jsonData["winsRight"];
 
             // Update the left side rounds won
-            // for (let i = 1; i <= 2; i++) {
-            //     document.getElementById(`leftRoundsWon${i}`).style.backgroundColor = winsLeft >= i ? "white" : "black";
-            // }
+             for (let i = 1; i <= 2; i++) {
+                 document.getElementById(`leftRoundsWon${i}`).style.backgroundColor = winsLeft >= i ? "white" : "black";
+             }
 
-            // // Update the right side rounds won
-            // for (let i = 1; i <= 2; i++) {
-            //     document.getElementById(`rightRoundsWon${i}`).style.backgroundColor = winsRight >= i ? "white" : "black";
-            // }
+        // Update the right side rounds won
+             for (let i = 1; i <= 2; i++) {
+             document.getElementById(`rightRoundsWon${i}`).style.backgroundColor = winsRight >= i ? "white" : "black";
+        }
 
             // Updates the Team Names and week
             document.getElementById("teamNameLeft").innerHTML = jsonData["teamNameLeft"];
