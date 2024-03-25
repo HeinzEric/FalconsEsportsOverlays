b0c6850f6d609aa5caee39d345e4488b4333b4b3
MADMAN-Modding <mad@kmdtech.net>
2024-03-25 10:08:38 -0400

    stuff

---
+++ app/bin/falcons_esports_overlay_controller.dart
@@ -0,0 +1,15 @@
+import 'json_reader.dart';
+import 'git_downloader.dart';
+
+void main() {
+  var jsonReader = JsonReader();
+  var gitDownloader = GitDownloader();
+
+  // Now you can work with jsonData, which will be a Map
+
+  // Calls methods from their respective classes
+  gitDownloader.repoCloner();
+  gitDownloader.update();
+  jsonReader.write();
+  // print(jsonReader.fullOutput());
+}
---
+++ app/bin/git_downloader.dart
@@ -0,0 +1,17 @@
+import 'dart:io';
+import 'package:rw_git/rw_git.dart';
+
+class GitDownloader {
+  RwGit rwGit = RwGit();
+
+  void repoCloner() {
+    rwGit.clone(
+        "D:\\", "https://github.com/HeinzEric/FalconsEsportsOverlays.git");
+  }
+
+  void update() {
+    Process.run('git', ['pull']).then((ProcessResult resutls) {
+      print(resutls.stdout);
+    });
+  }
+}
