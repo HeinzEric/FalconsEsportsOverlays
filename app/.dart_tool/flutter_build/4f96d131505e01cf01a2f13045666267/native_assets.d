77eba799a2c9b174af3672ebaaffa32dd419dc55
MADMAN-Modding <mad@kmdtech.net>
2024-03-25 10:08:33 -0400

    don't worry about it

--- app/.dart_tool/flutter_build/4f96d131505e01cf01a2f13045666267/native_assets.d
+++
@@ -1,1 +0,0 @@
- D:\\Coding\\Projects\\FalconsEsportsOverlays\\app\\.dart_tool\\flutter_build\\4f96d131505e01cf01a2f13045666267\\native_assets.yaml: 
--- app/android/app/src/main/kotlin/com/example/falcons_esports_overlays_controller/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.falcons_esports_overlays_controller
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity()
